27377057ecdae4bfb6c63780518bce8a668ae4cb
w30031905 <wangbingquan6@huawei.com>
2024-12-31 16:55:28 +0800

    测试unshallow

--- app/src/main/java/com/coolweather/android/NewMainActivity.java
+++ app/src/main/java/com/coolweather/android/NewMainActivity.java
@@ -9,6 +9,6 @@ public class NewMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("新增类测试");
+        System.out.println("新增类测试2");
     }
 }
